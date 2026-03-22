4f40244fdc0c7cc778e07c8607de4e5aa4299648
ashi100sh <ashi100sh@gmail.com>
2026-03-23 01:04:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VER    = 'gjv-v6.3';
+const VER    = 'gjv-v6.8';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 // Install — cache all assets, activate immediately
